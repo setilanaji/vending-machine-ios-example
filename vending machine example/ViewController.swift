0024e98581d301608bd076b3927e098f191d9b54
setilanaji <setyajiyudha266@gmail.com>
2021-07-26 10:36:52 +0700

    Initial Commit

---
+++ vending machine example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vending machine example
+//
+//  Created by Yudha S on 2021/7/26.
+//  Copyright © 2021 Macx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
